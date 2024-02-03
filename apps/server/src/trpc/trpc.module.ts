ca0812cf6b7b46264c923f4ca8f2d0f3fbc59857
lukaasz555 <lukasz.zegar555@gmail.com>
2024-02-03 22:01:51 +0100

    trpc router

--- apps/server/src/trpc/trpc.module.ts
+++ apps/server/src/trpc/trpc.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TrpcService } from './trpc.service';
+import { TrpcRouter } from './trpc.router';
 
 @Module({
     imports: [],
-    providers: []
+    providers: [TrpcService, TrpcRouter]
 })
 export class TrpcModule {}
